de779078bd107d1fda34a3a298398742e208e78d
Sam Olivier <smsolivier@gmail.com>
2018-05-06 23:42:13 -0700

    added getenv for turning off wtimer

--- utils/chtimer/WTimer.cpp
+++ utils/chtimer/WTimer.cpp
@@ -6,6 +6,7 @@ WTimer::WTimer() {
 }
 
 WTimer::~WTimer() {
+	if (getenv("NWTIMER") != NULL) return; 
 	m_el = chrono::system_clock::now() - m_start; 
 	cout << "Wall Time = " << m_el.count() << " seconds" << endl; 
 }
