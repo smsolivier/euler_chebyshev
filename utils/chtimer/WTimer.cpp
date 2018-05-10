cb4ddc28dcec220c7657340969ecbcc84327dec8
Sam Olivier <smsolivier@gmail.com>
2018-05-09 17:03:52 -0700

    output minuted if over 60 seconds

--- utils/chtimer/WTimer.cpp
+++ utils/chtimer/WTimer.cpp
@@ -8,5 +8,9 @@ WTimer::WTimer() {
 WTimer::~WTimer() {
 	if (getenv("NWTIMER") != NULL) return; 
 	m_el = chrono::system_clock::now() - m_start; 
-	cout << "Wall Time = " << m_el.count() << " seconds" << endl; 
+	if (m_el.count() > 60) {
+		cout << "Wall Time = " << m_el.count()/60 << " minutes" << endl; 
+	} else {
+		cout << "Wall Time = " << m_el.count() << " seconds" << endl; 
+	}
 }
