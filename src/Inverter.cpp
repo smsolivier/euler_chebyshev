048a9aeaef7629620c9664a823f4e8257aae6966
Sam Olivier <smsolivier@gmail.com>
2018-05-09 18:02:17 -0700

    comments

--- src/Inverter.cpp
+++ src/Inverter.cpp
@@ -1,8 +1,5 @@
 #include "Inverter.H"
 
-#define DIRICHLET
-// #define NEUMANN
-
 Inverter::Inverter() {
 	m_init = false; 
 }
@@ -14,8 +11,8 @@ void Inverter::init(array<int,DIM> N, int BC) {
 	m_init = true; 
 	m_N = N; 
 
+	// compute second derivative matrix 
 	m_D2.resize(m_N[2], m_N[2]); 
-
 	double c; 
 	for (int i=0; i<m_N[2]; i++) {
 		if (i==0) c = 2; 
@@ -27,18 +24,25 @@ void Inverter::init(array<int,DIM> N, int BC) {
 		}
 	}
 
+	// store solver object and matrices 
 	m_ppc = new PaperCutter*[m_N[0]*m_N[1]]; 
 	m_theta = new Eigen::SparseMatrix<cdouble>*[m_N[0]*m_N[1]]; 
+
+	// run LU factorization on all matrices 
 	#pragma omp parallel for 
 	for (int i=0; i<m_N[0]; i++) {
 		double m2 = pow(freq(i,0), 2); 
 		for (int j=0; j<m_N[1]; j++) {
 			int index = j+i*m_N[1]; 
 			double n2 = pow(freq(j,1), 2); 
+
+			// compute theta 
 			m_theta[index] = new Eigen::SparseMatrix<cdouble>(m_N[2], m_N[2]); 
-			*m_theta[index] = m_D2; 
+			*m_theta[index] = m_D2; // set to second derivative 
 			for (int k=0; k<m_N[2]; k++) {
-				m_theta[index]->coeffRef(k,k) -= (m2 + n2); 
+				m_theta[index]->coeffRef(k,k) -= (m2 + n2); // set to theta 
+
+				// set boundary conditions 
 				if (BC == 0) { // double neumann 
 					m_theta[index]->coeffRef(m_N[2]-1,k) = pow(k,2); 
 					m_theta[index]->coeffRef(m_N[2]-2,k) = pow(k,2)*pow(-1, k+1); 					
@@ -52,6 +56,8 @@ void Inverter::init(array<int,DIM> N, int BC) {
 					// theta.coeffRef(m_N[2]-2,k) = pow(-1,k); 
 				}
 			}
+
+			// initialize solver object 
 			m_ppc[index] = new PaperCutter; 
 			m_ppc[index]->init(m_theta[index]); 
 		}
@@ -83,17 +89,25 @@ void Inverter::invert(Scalar& scalar, Vector& V) {
 		#pragma omp for 
 		for (int i=0; i<m_N[0]; i++) {
 			for (int j=0; j<m_N[1]; j++) {
-				int index = j + i*m_N[1]; 
+				int index = j + i*m_N[1]; // flatten 
+
+				// copy to Eigen RHS vector 
 				for (int k=0; k<m_N[2]; k++) {
 					rhs[k] = orig(i,j,k); 
 				}
+
+				// compute V_z(z=+-1) (in physical space) 
 				rhs[m_N[2]-1] = 0; 
 				rhs[m_N[2]-2] = 0; 
 				for (int k=0; k<m_N[2]; k++) {
 					rhs[m_N[2]-2] += V[2](i,j,k) * pow(-1., k); 
 					rhs[m_N[2]-1] += V[2](i,j,k); 
 				}
+
+				// call matrix solver 
 				m_ppc[index]->solve(rhs, sol); 
+
+				// copy from Eigen to Scalar 
 				for (int k=0; k<m_N[2]; k++) {
 					scalar(i,j,k) = sol[k]; 
 				}
@@ -115,10 +129,16 @@ void Inverter::invert(Scalar& scalar) {
 		for (int i=0; i<m_N[0]; i++) {
 			for (int j=0; j<m_N[1]; j++) {
 				int index = j + i*m_N[1]; 
+
+				// copy to Eigen RHS vector 
 				for (int k=0; k<m_N[2]; k++) {
 					rhs[k] = orig(i,j,k); 
 				}
+
+				// call solver object 
 				m_ppc[index]->solve(rhs, sol); 
+				
+				// copy back to Scalar 
 				for (int k=0; k<m_N[2]; k++) {
 					scalar(i,j,k) = sol[k]; 
 				}
--- src/PaperCutter.cpp
+++ src/PaperCutter.cpp
@@ -3,33 +3,43 @@
 PaperCutter::PaperCutter() {}
 
 void PaperCutter::init(Eigen::SparseMatrix<cdouble>* theta) {
-	m_theta = theta; 
+	m_theta = theta; // save pointer to matrix 
 
 #ifdef PC 
+	// get reduced matrix 
 	m_M = m_theta->block(0,2, m_theta->rows()-2, m_theta->cols()-2); 
+	// factorize 
 	m_solver.compute(m_M); 
-	CHECK(m_solver.info() == 0, "solver issue :("); 
+	CHECK(m_solver.info() == 0, "solver issue :("); // check for solver issues 
 
+	// get L column
 	Eigen::VectorXcd L = m_theta->block(0,0, m_theta->rows()-2, 1); 
 	m_L = m_solver.solve(L); 
 
+	// get R column 
 	Eigen::VectorXcd R = m_theta->block(0,1, m_theta->rows()-2, 1); 
 	m_R = m_solver.solve(R); 
 
+	// get T and B rows 
 	m_T = m_theta->block(m_theta->rows()-2,2, 1, m_theta->cols()-2); 
 	m_B = m_theta->block(m_theta->rows()-1,2, 1, m_theta->cols()-2); 
 
+	// get values in intersection of paper cuts 
 	m_vals[0] = m_theta->coeff(m_theta->rows()-2,0); 
 	m_vals[1] = m_theta->coeff(m_theta->rows()-2,1); 
 	m_vals[2] = m_theta->coeff(m_theta->rows()-1,0); 
 	m_vals[3] = m_theta->coeff(m_theta->rows()-1,1); 
 
+	// compute 2x2 coefficients 
 	cdouble a = m_vals[0] - m_T.dot(m_L); 
 	cdouble b = m_vals[1] - m_T.dot(m_R); 
 	cdouble c = m_vals[2] - m_B.dot(m_L); 
 	cdouble d = m_vals[3] - m_B.dot(m_R); 
 
+	// get determinant 
 	cdouble det = a*d - b*c; 
+
+	// if singular skip solving 
 	m_skip = false; 
 	if (det == 0. || m_solver.info() != 0) {
 		m_skip = true; 
@@ -41,6 +51,7 @@ void PaperCutter::init(Eigen::SparseMatrix<cdouble>* theta) {
 	m_inv[2] = -c/det; 
 	m_inv[3] = a/det; 
 #else 
+	// use regular LU 
 	m_solver.compute(*m_theta); 
 #endif
 }
@@ -54,26 +65,31 @@ void PaperCutter::solve(Eigen::VectorXcd& rhs, Eigen::VectorXcd& sol) {
 		return; 
 	}
 
+	// get reduced rhs 
 	Eigen::VectorXcd g(m_M.rows()); 
 	for (int i=0; i<g.rows(); i++) {
 		g[i] = rhs[i]; 
 	}
 
+	// solve rhs 
 	Eigen::VectorXcd gt = m_solver.solve(g); 
 
+	// compute left over g's 
 	cdouble g0 = rhs[rhs.rows()-2] - m_T.dot(gt); 
 	cdouble g1 = rhs[rhs.rows()-1] - m_B.dot(gt); 
 
+	// compute left over solutions 
 	cdouble f0 = m_inv[0]*g0 + m_inv[1]*g1; 
 	cdouble f1 = m_inv[2]*g0 + m_inv[3]*g1; 
 
+	// set solution 
 	sol[0] = f0; 
 	sol[1] = f1; 
-
 	for (int i=0; i<rhs.rows()-2; i++) {
 		sol[i+2] = gt[i] - f0*m_L[i] - f1*m_R[i]; 
 	}
 #else 
+	// run regular solve 
 	if (m_solver.info() == 0) 
 		sol = m_solver.solve(rhs); 
 	else {
@@ -84,6 +100,7 @@ void PaperCutter::solve(Eigen::VectorXcd& rhs, Eigen::VectorXcd& sol) {
 	}
 #endif
 
+	// make sure answer is acceptable 
 	#ifdef ErrorCheck 
 	for (int i=0; i<m_M.rows(); i++) {
 		CHECK(!isnan(sol[i].real()), "nan"); 
