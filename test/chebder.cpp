6592b405087af463e17bf10309d9ec4ad69129ca
Sam Olivier <smsolivier@gmail.com>
2018-05-09 22:02:19 -0700

    chebyshev derivative test

--- test/chebder.cpp
+++ test/chebder.cpp
@@ -3,38 +3,42 @@
 
 using namespace std; 
 
-int main() {
+int main(int argc, char* argv[]) {
 	int N = 8; 
+	if (argc > 1) N = atoi(argv[1]); 
 	vector<cdouble> f(N); 
 
 	Cheb1D cheb(N, 1, &f[0]); 
 
-	vector<double> theta(N), z(N); 
+	vector<cdouble> theta(N), z(N); 
 
 	for (int i=0; i<N; i++) {
 		theta[i] = i*M_PI/(N-1); 
 		z[i] = cos(theta[i]); 
-		// f[i] = 2*z[i]*z[i] - 1; // T2 
 		f[i] = z[i]*z[i]*z[i]; 
-		// f[i] = z[i]*z[i] + z[i]*z[i]*z[i]; 
 	}
+
+	vector<cdouble> d(N), d2(N); 
 
 	cheb.transform(&f[0], -1); 
+	cheb.deriv(&f[0], &d[0]); 
+	cheb.transform(&d[0], 1); 
 
-	cheb.deriv(&f[0]); 
-
-	cheb.transform(&f[0], 1); 
-
+	bool wrong = false; 
 	for (int i=0; i<N; i++) {
-		cout << f[i].real() << endl; 
+		if (abs(d[i] - 3.*z[i]*z[i]) > 1e-3) wrong = true; 
 	}
-	cout << endl; 
+	if (wrong) cout << "first derivative: WRONG!" << endl; 
+	else cout << "first derivative: my man!" << endl; 
 
-	cheb.transform(&f[0], -1); 
-	cheb.deriv(&f[0]); 
-	cheb.transform(&f[0], 1); 
+	cheb.transform(&d[0], -1); 
+	cheb.deriv(&d[0], &d2[0]); 
+	cheb.transform(&d2[0], 1); 
 
+	wrong = false; 
 	for (int i=0; i<N; i++) {
-		cout << f[i].real() << endl; 
+		if (abs(d2[i] - 6.*z[i]) > 1e-3) wrong = true; 
 	}
+	if (wrong) cout << "second derivative: WRONG!" << endl; 
+	else cout << "second derivate: my man!" << endl; 
 }
